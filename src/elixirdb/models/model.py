a8604aa397ae43c6ba40d3b46af437d06f1a7055
Victor Nguyen <victor.win86@gmail.com>
2025-02-05 21:22:41 -0500

    Updated RowMapping import

--- src/elixirdb/models/model.py
+++ src/elixirdb/models/model.py
@@ -16,7 +16,6 @@ class StrictModel(BaseModel):
     # We allow here because we want to provide the validation error at the end
     model_config = ConfigDict(
         extra="forbid",
-        validate_assignment=True,
         arbitrary_types_allowed=True,
         str_strip_whitespace=True,
         use_enum_values=True,
